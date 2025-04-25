44dcbc6363740b88832336bbd5ee03315c27ed4c
andrea.sibona <andrea.sibona@check24.de>
2025-04-25 10:04:22 +0200

    more stuff - I should really give meaningful names

--- lua/custom/spring_tools.lua
+++ lua/custom/spring_tools.lua
@@ -4,8 +4,14 @@ local M = {}
 function M.config()
   -- TODO: Maybe add it to lspconfig.config
   local capabilities = vim.lsp.protocol.make_client_capabilities()
+  -- TODO: move outside, I've already been bamboozled once
   capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
-  capabilities = vim.tbl_deep_extend('keep', capabilities, { workspace = { executeCommand = { dynamicRegistration = true } } })
+  capabilities = vim.tbl_deep_extend('keep', capabilities, {
+    workspace = {
+      executeCommand = { dynamicRegistration = true },
+      didChangeWorkspaceFolders = { dynamicRegistration = true },
+    },
+  })
   -- Add custom springboot_ls manually since it's not in mason
   vim.lsp.config['springboot_ls'] = {
     cmd = {
@@ -16,11 +22,12 @@ function M.config()
       '-jar',
       vim.fn.expand '~/.local/share/lsp/spring-boot-language-server.jar',
     },
-    filetypes = { 'properties', 'yaml', 'java' }, -- optionally add 'java' if you want it in Java files too
-    root_markers = { 'gradle.build', 'pom.xml', '.git/' },
+    filetypes = { 'properties', 'yaml', 'java' },
+    root_markers = { 'build.gradle', 'pom.xml', '.git/' },
     settings = {},
     capabilities = capabilities,
   }
+  -- vim.lsp.enable 'springboot_ls'
   --vim.lsp.set_log_level 'debug'
 end
 
@@ -34,7 +41,10 @@ function M.on_attach(event)
   --  requests/notifications, not client-to-server.)
   client.handlers['sts/addClasspathListener'] = function(err, result, ctx)
     --vim.notify('Received sts/addClasspathListener Command' .. vim.inspect(result), vim.log.levels.INFO, {})
-    local clients = vim.lsp.get_clients { bufnr = ctx.bufnr, name = 'jdtls' }
+    local clients = vim.lsp.get_clients {
+      --bufnr = ctx.bufnr,
+      name = 'jdtls',
+    }
     if #clients == 0 then
       vim.notify('No active jdtls client found, sts needs one running', vim.log.levels.ERROR, {})
       return {
@@ -45,14 +55,7 @@ function M.on_attach(event)
       jdtls_client:request('workspace/executeCommand', {
         command = 'sts.java.addClasspathListener',
         arguments = { result.callbackCommandId },
-      }, function(err, result, ctx)
-        vim.notify('sent executeCommand, received' .. vim.inspect(result), vim.log.levels.INFO, {})
-      end, ctx.bufnr)
-      -- jdtls_client:exec_cmd({
-      --   title = 'addClasspathListener',
-      --   command = 'sts.java.addClasspathListener',
-      --   arguments = { result.callbackCommandId },
-      -- }, ctx, nil)
+      }, function(err, result, ctx) end, ctx.bufnr)
     end
 
     return {
@@ -60,7 +63,7 @@ function M.on_attach(event)
     }
   end
   client.handlers['sts/removeClasspathListener'] = function(err, result, ctx)
-    vim.notify('Received sts/removeClasspathListener Command', vim.log.levels.INFO, {})
+    --vim.notify('Received sts/removeClasspathListener Command', vim.log.levels.INFO, {})
     local clients = vim.lsp.get_clients { bufnr = ctx.bufnr, name = 'jdtls' }
     if #clients == 0 then
       vim.notify('No active jdtls client found, sts needs one running', vim.log.levels.ERROR, {})
@@ -72,14 +75,7 @@ function M.on_attach(event)
       jdtls_client:request('workspace/executeCommand', {
         command = 'sts.java.removeClasspathListener',
         arguments = { result.callbackCommandId },
-      }, function(err, result, ctx)
-        vim.notify('sent executeCommand, received' .. vim.inspect(result), vim.log.levels.INFO, {})
-      end, ctx.bufnr)
-      -- jdtls_client:exec_cmd({
-      --   title = 'removeClasspathListener',
-      --   command = 'sts.java.removeClasspathListener',
-      --   arguments = { result.callbackCommandId },
-      -- }, ctx, nil)
+      }, function(err, result, ctx) end, ctx.bufnr)
     end
     return { result = { success = true } }
   end
@@ -92,25 +88,20 @@ function M.on_attach(event)
       -- only register this back and forth for jdtls and springboot_ls
       -- again, maybe in lsp.config in the future?? Or is it only for lsp.start
       if client.id ~= ac_client.id and (ac_client.name == 'springboot_ls' or ac_client.name == 'jdtls') then
-        print('handling client ' .. ac_client.name .. ' for ' .. result.command)
-        ac_client:exec_cmd(result, ctx, function(err, result, ctx)
-          if err ~= nil then
-            print('ohno' .. vim.inspect(err))
-            coroutine.resume(co, {
-              err = err,
-            })
-          else
-            print('ohye' .. vim.inspect(result))
-            coroutine.resume(co { result = result })
-          end
-        end)
+        -- seems I cannot use exec_cmd because of not very dynamic registration of execute commands
+        --ac_client:exec_cmd(result, ctx, function(err, result, ctx)
+        ac_client:request('workspace/executeCommand', result, function(err, result, ctx)
+          coroutine.resume(co, {
+            err = err,
+            result = result,
+          })
+        end, ctx.bufnr)
         break
       end
     end
 
     -- blocks the current coroutine to wait for the exec_cmd result
     local ret = coroutine.yield()
-    print(vim.inspect(co) .. 'resumes' .. vim.inspect(ret))
     return ret
   end
 end
